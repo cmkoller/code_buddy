aa4dc0902c1b30c12093a41509fd2d01a3a9e007
cmkoller <cmkoller01@gmail.com>
2014-12-29 16:10:10 -0500

    Added test for user being signed in before submitting content

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,6 @@
 class  ResourcesController < ApplicationController
   # before_action :find_resource, only: [:show, :edit, :update, :destroy]
-  # before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @resources = Resource.all
--- spec/features/user_submits_resource_spec.rb
+++ spec/features/user_submits_resource_spec.rb
@@ -13,23 +13,33 @@ Acceptance Criteria
 
 ) do
 
-  # user = FactoryGirl.create(:user)
-  #
-  # before(:each) do
-  #   visit new_user_session_path
-  #   fill_in "Email", with: user.email
-  #   fill_in "Password", with: user.password
-  #   click_button "Log in"
-  #   visit edit_user_registration_path
-  # end
-  #
-  scenario 'user successfully submits a resource' do
-    visit new_resource_path
-    fill_in 'Title', :with => "Descriptive Title"
-    fill_in 'Description', :with => "this resource is very descriptive"
-    fill_in 'Url', :with => "http://www.google.com"
+  context "authenticated user" do
+    let(:user){ FactoryGirl.create(:user) }
 
-    click_button 'Submit Resource'
-    expect(page).to have_content("You've successfully submitted a resource!")
+    before(:each) do
+      visit new_user_session_path
+      fill_in "Email", with: user.email
+      fill_in "Password", with: user.password
+      click_button "Log in"
+      visit edit_user_registration_path
+    end
+
+    scenario 'user successfully submits a resource' do
+
+      visit new_resource_path
+      fill_in 'Title', :with => "Descriptive Title"
+      fill_in 'Description', :with => "this resource is very descriptive"
+      fill_in 'Url', :with => "http://www.google.com"
+
+      click_button 'Submit Resource'
+      expect(page).to have_content("You've successfully submitted a resource!")
+    end
+  end
+
+  context "unauthenticated user" do
+    scenario 'user tries to submit resource when not signed in' do
+      visit new_resource_path
+      expect(page).to have_content("You need to sign in or sign up before continuing.")
+    end
   end
 end
