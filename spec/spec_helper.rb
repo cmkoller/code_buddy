1c769a7287457c8055ed414fb37a227983127940
Christina Koller <cmkoller01@gmail.com>
2014-12-30 11:15:16 -0500

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,8 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require 'coveralls'
-Coveralls.wear!('rails')
+require "coveralls"
+Coveralls.wear!("rails")
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
