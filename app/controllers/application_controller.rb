11a4ba569a9df9ed64f4a0a350bbe63db288833a
cmkoller <cmkoller01@gmail.com>
2014-12-31 11:10:15 -0500

    Un-broke the configure devise permitted parameters in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameters
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
@@ -11,4 +11,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name << :display_name
     devise_parameter_sanitizer.for(:account_update) << :first_name << :last_name << :display_name
   end
+
 end
