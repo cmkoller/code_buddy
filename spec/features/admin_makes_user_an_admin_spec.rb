1552930477e0ffcd68202a73b0cbdbcf336b5252
cmkoller <cmkoller01@gmail.com>
2015-01-05 14:02:41 -0500

    Added test to make sure non-admin cannot admin-infy other users

--- spec/features/admin_makes_user_an_admin_spec.rb
+++ spec/features/admin_makes_user_an_admin_spec.rb
@@ -29,54 +29,23 @@ Acceptance Criteria
       expect(page).to have_content("#{user.display_name} is now an admin!")
     end
 
-    # scenario 'admin deletes a user' do
-    #   user.save
-    #   visit admin_users_path
-    #   click_link user.display_name
-    #   click_button "Delete"
-    #   expect(page).to have_content("User successfully deleted")
-    #   expect(page).to have_no_content(user.display_name)
-    # end
-    #
-    # scenario 'admin deletes a buddy' do
-    #   visit buddy_path(buddy)
-    #   click_link "Delete"
-    #   expect(page).to have_content("You've successfully deleted a buddy!")
-    # end
   end
 
-  # context "User is not admin" do
-  #
-  #   before(:each) do
-  #     visit new_user_session_path
-  #     fill_in "user[email]", with: user.email
-  #     fill_in "user[password]", with: user.password
-  #     click_button "Log in"
-  #   end
-  #
-  #   scenario 'normal user cannot view list of users' do
-  #     visit admin_users_path
-  #     expect(page).to have_content("You are not authorized to do this")
-  #     expect(page).to have_no_content("Users")
-  #   end
-  #
-  #   scenario 'normal user cannot delete a user' do
-  #     user2 = user
-  #     user2.display_name = "Joe"
-  #     user2.save
-  #     visit user_path(user2)
-  #     expect(page).to have_no_button("Delete")
-  #     page.driver.submit :delete, admin_user_path(user2), {}
-  #     expect(page).to have_content("You are not authorized to do this")
-  #     expect(page).to have_content(user2.display_name)
-  #   end
-  #
-  #   scenario 'normal user cannot delete a buddy' do
-  #     visit buddy_path(buddy)
-  #     expect(page).to have_no_content("Delete")
-  #     page.driver.submit :delete, buddy_path(buddy), {}
-  #     expect(page).to have_content("You are not authorized to do this")
-  #   end
-  #
-  # end
+  context "User is not admin" do
+
+    before(:each) do
+      visit new_user_session_path
+      fill_in "user[email]", with: user.email
+      fill_in "user[password]", with: user.password
+      click_button "Log in"
+    end
+
+    scenario "normal user cannot make another user into an admin" do
+      visit user_path(user)
+      expect(page).to have_no_content("Make Admin")
+      page.driver.submit :patch, admin_user_path(user), {admin: true}
+      expect(page).to have_content("You are not authorized to do this")
+    end
+  end
+
 end
