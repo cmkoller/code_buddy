021a06b4094aa67bbe3d73a3ae0aa63aa129503e
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2015-01-06 12:52:30 -0500

    changed pagination to show 10 buddies per page

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -2,7 +2,7 @@ class  BuddiesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @buddies = Buddy.all.page(params[:page]).per(1)
+    @buddies = Buddy.all.page(params[:page]).per(10)
   end
 
   def new
