c3921d26bbf45eb57ffb7187f0bd4f82e87b76d6
john ko <johnsko89@gmail.com>
2015-01-06 13:27:15 -0500

    added votes/migration

---
+++ db/migrate/20150106182416_add_votes.rb
@@ -0,0 +1,10 @@
+class AddVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.integer :user_id, null: false
+      t.integer :review_id, null: false
+      t.integer :vote, null: false
+    end
+      add_index :votes, :review_id
+  end
+end
