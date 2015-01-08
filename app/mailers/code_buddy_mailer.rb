0fda5b61f8bb43c868bada288b179582483ed8eb
cmkoller <cmkoller01@gmail.com>
2015-01-08 16:54:07 -0500

    Deleted unneccessary file

--- app/mailers/code_buddy_mailer.rb
+++
@@ -1,9 +0,0 @@
-class CodeBuddyMailer < ActionMailer::Base
-  default from: "from@example.com"
-
-  def review_posted(current_user, review)
-    mail to: current_user.email,
-      subject: 'Parking Confirmation'
-  end
-
-end
